e2c51c5ee40fdf5e5ebadfbe02936564923b7b49
BachShinichiroe <kitchen.number@gmail.com>
2021-02-17 17:23:00 +0900

    add:レスポンシブ

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,16 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery/dist/jquery.js
+
 //= require_tree .
+
+
+
+$(function() {
+   const hum = $('#hamburger, .close')
+   const nav = $('.sp-nav')
+   hum.on('click', function(){
+      nav.toggleClass('toggle');
+   });
+});
